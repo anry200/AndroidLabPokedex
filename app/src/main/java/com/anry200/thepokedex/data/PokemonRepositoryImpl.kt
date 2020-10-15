5590b4b8269a4f4afa3129bf2ce94c68bbcaaf6b
DenysHoncharenko <anry200@gmail.com>
2020-10-15 11:23:18 +0300

    Lesson 4: Repository add cache

--- app/src/main/java/com/anry200/thepokedex/data/PokemonRepositoryImpl.kt
+++ app/src/main/java/com/anry200/thepokedex/data/PokemonRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.anry200.thepokedex.data
 
+import android.content.ClipData
 import com.anry200.thepokedex.domain.Pokemon
 import com.anry200.thepokedex.domain.PokemonDetails
 import com.anry200.thepokedex.domain.PokemonRepository
@@ -9,6 +10,9 @@ import retrofit2.Response
 
 class PokemonRepositoryImpl : PokemonRepository {
     private val api: PokedexApiService = createPokedexApiService() //TODO: use DI
+
+    //1. when cache is not valid
+    private val cache = mutableMapOf<String, Pair<Long,PokemonDetails>>()
 
     override fun getPokemonList(callback: PokemonRepository.ApiCallback<List<Pokemon>>) {
         api.fetchPokemonList().enqueue(object : Callback<PokemonListResponse> {
@@ -34,28 +38,38 @@ class PokemonRepositoryImpl : PokemonRepository {
     }
 
     override fun getPokemonById(id: String, callback: PokemonRepository.ApiCallback<PokemonDetails>) {
-        api.fetchPokemonInfo(id).enqueue(object : Callback<PokemonInfoResponse> {
-            override fun onResponse(call: Call<PokemonInfoResponse>, response: Response<PokemonInfoResponse>) {
-                val pokemonInfo = response.body()
+        val value = cache.get(id) //check cache
 
-                if (response.isSuccessful && pokemonInfo != null) {
-                    val abilities = pokemonInfo.abilities.map {
-                        it.ability.name
+        if (value != null && value.first + 10 * 60 * 1000 < System.currentTimeMillis() ) {
+            callback.onSuccess(value.second)
+        } else {
+            api.fetchPokemonInfo(id).enqueue(object : Callback<PokemonInfoResponse> {
+                override fun onResponse(call: Call<PokemonInfoResponse>, response: Response<PokemonInfoResponse>) {
+                    val pokemonInfo = response.body()
+
+                    if (response.isSuccessful && pokemonInfo != null) {
+                        val abilities = pokemonInfo.abilities.map {
+                            it.ability.name
+                        }
+
+                        val pokemonDetails = PokemonDetails(
+                            pokemonInfo.id, pokemonInfo.name, pokemonInfo.imageUrl, abilities
+                        )
+
+                        cache.put(pokemonInfo.id, Pair(System.currentTimeMillis(),pokemonDetails) ) //save into cache
+
+                        callback.onSuccess(pokemonDetails)
+                    } else {
+                        callback.onError()
                     }
+                }
 
-                    val pokemonDetails = PokemonDetails(
-                        pokemonInfo.id, pokemonInfo.name, pokemonInfo.imageUrl, abilities
-                    )
-
-                    callback.onSuccess(pokemonDetails)
-                } else {
+                override fun onFailure(call: Call<PokemonInfoResponse>, t: Throwable) {
                     callback.onError()
                 }
-            }
+            })
+        }
 
-            override fun onFailure(call: Call<PokemonInfoResponse>, t: Throwable) {
-                callback.onError()
-            }
-        })
+
     }
 }
