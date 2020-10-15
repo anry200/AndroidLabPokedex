19c081879b13ecff2dc5c0e173ce715cfc3c2d99
DenysHoncharenko <anry200@gmail.com>
2020-10-15 12:01:55 +0300

    Lesson 5: Added Master/Details flow

--- app/src/main/java/com/anry200/thepokedex/presentation/details/PokemonDetailsActivity.kt
+++ app/src/main/java/com/anry200/thepokedex/presentation/details/PokemonDetailsActivity.kt
@@ -3,19 +3,44 @@ package com.anry200.thepokedex.presentation.details
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.anry200.thepokedex.R
+import com.anry200.thepokedex.domain.PokemonDetails
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_pokemon_details.abilities
+import kotlinx.android.synthetic.main.activity_pokemon_details.image
 import kotlinx.android.synthetic.main.item_pokemon.name
 
 const val PARAM_POKEMON_ID = "param.pokemon.id"
 class PokemonDetailsActivity : AppCompatActivity() {
+    val viewModel by viewModels<PokemonDetailsViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon_details)
 
-        val pokemonId = intent.getStringExtra(PARAM_POKEMON_ID)
+        val pokemonId = intent.getStringExtra(PARAM_POKEMON_ID) ?: "1" //TODO: add error handling
 
-        name.text = pokemonId
+        viewModel.pokemonDetailsLiveData.observe(this, Observer { pokemonDetails ->
+            if (pokemonDetails != null) {
+                showPokemonDetails(pokemonDetails)
+            }
+        })
+
+        viewModel.loadPokemonData(pokemonId)
+    }
+
+    private fun showPokemonDetails(pokemon: PokemonDetails) {
+        name.text = pokemon.name
+
+        val abilitiesNames: String = pokemon.abilities.joinToString { it }
+        abilities.text = abilitiesNames
+
+        Picasso.get()
+            .load(pokemon.imageUrl)
+            .into(image)
     }
 
     companion object {
---
+++ app/src/main/java/com/anry200/thepokedex/presentation/details/PokemonDetailsViewModel.kt
@@ -0,0 +1,29 @@
+package com.anry200.thepokedex.presentation.details
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.anry200.thepokedex.data.PokemonRepositoryImpl
+import com.anry200.thepokedex.domain.PokemonDetails
+import com.anry200.thepokedex.domain.PokemonRepository
+
+class PokemonDetailsViewModel : ViewModel() {
+    val repository: PokemonRepository = PokemonRepositoryImpl()
+
+    private val _pokemonDetailsLiveData = MutableLiveData<PokemonDetails>()
+    val pokemonDetailsLiveData: LiveData<PokemonDetails> = _pokemonDetailsLiveData
+
+    fun loadPokemonData(id: String) {
+        //TODO: add Loading
+
+        repository.getPokemonById(id, object: PokemonRepository.ApiCallback<PokemonDetails> {
+            override fun onSuccess(data: PokemonDetails) {
+                _pokemonDetailsLiveData.postValue(data)
+            }
+
+            override fun onError() {
+               //TODO: handle error
+            }
+        })
+    }
+}
